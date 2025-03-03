b93e190c33c8022fe7ebcced215f088f0ebd2249
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-03-03 20:37:28 -0300

    feat: Initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [MainComponent],
+  template: `<app-main></app-main>`,
 })
 export class AppComponent {
   title = 'desafio-angular';
